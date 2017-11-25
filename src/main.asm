cc16ac4acbbef92d9e8950701260a7e71a7804ee
João Pedro <jsangregorio@hotmail.com>
2017-11-25 10:31:29 -0200

    Movimentação

--- src/main.asm
+++ src/main.asm
@@ -3,45 +3,61 @@ Include ..\Irvine32.inc
 
 .data
 	;mapa byte 119 DUP(32), 0 ; string de caracteres do chão
+    PosicaoX byte 0,0
+    PosicaoY byte 0,0
 
 .code
 main PROC
 
-    mov  eax,50          ; sleep, to allow OS to time slice
-    call Delay           ; (otherwise, some key presses are lost)
+    mov  eax,20          
+    call Delay           
     call Mover
-    int 3
+    call ImprimirPersonagem
     jmp main
 
     exit
     main endp
 Mover PROC
     call ReadKey         ; look for keyboard input
-    jz   main      ; no key pressed yet
-
+    je   FimMove      ; no key pressed yet
     cmp dx,VK_UP  ; Indo pra cima?
         jne MexerBaixo
-        mov eax, 1 ; pra teste
-        ; Mover pra cima
-        ret 
+        dec PosicaoY
+        jmp FimMove
     MexerBaixo:
     cmp dx,VK_DOWN  ; Indo pra baixo?
         jne MexerEsq
-        mov eax, 2
-        ; Mover pra baixo
-        ret 
+        inc PosicaoY
+        jmp FimMove
     MexerEsq:
     cmp dx,VK_LEFT  ; Indo pra esquerda?
         jne MexerDireita
-        mov eax, 3
-        ; Mover esquerda 
-        ret 
+        dec PosicaoX
+        jmp FimMove
     MexerDireita:
     cmp dx,VK_RIGHT  ; Indo pra direita?
         jne FimMove
-        mov eax, 4
-        ; Mover pra direita
-        FimMove:
-        ret
+        inc PosicaoX
+    FimMove:
+    ret
 Mover endp
+ImprimirPersonagem PROC
+    ;Apagar a posicão antiga
+    mov dl, [PosicaoX+1]
+    mov dh, [PosicaoY+1]
+    call Gotoxy
+    mov al, 32 
+    call WriteChar
+    ; Escrever na posição nova
+    mov dl, PosicaoX
+    mov dh, PosicaoY
+    call Gotoxy
+    mov al, 254
+    call WriteChar
+    ; Salvar a posicao antiga
+    mov PosicaoX+1, dl
+    mov PosicaoY+1, dh
+    fimMove:
+    ret
+ImprimirPersonagem endp
 end main
