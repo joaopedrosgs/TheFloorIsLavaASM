b572be5c2e8b725f0916f7ed2fae55dfd796619d
João Pedro <jsangregorio@hotmail.com>
2017-12-08 19:36:29 -0200

    Pré-lava

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,8 @@
 Include ..\Irvine32.inc
 		  
-ImprimirEm MACRO X, Y, CARACTERE
+mImprimirEm MACRO X, Y, CARACTERE
+	xor edx, edx
+	xor eax, eax
 	mov dl, X
 	mov dh, Y
 	call Gotoxy
@@ -8,28 +10,39 @@ ImprimirEm MACRO X, Y, CARACTERE
 	call WriteChar
 ENDM   
 
+mGerarAleatorio8 MACRO largura, dest
+		call Randomize
+		xor eax,eax
+		mov al, largura
+		call RandomRange ; de 0 até largura-padding
+		add al, PADDING/2 ; Largura = Largura + padding/2
+		mov  dest,al
+endm
+
 CARACTERE_PERSONAGEM = 254
 CARACTERE_ESPACO = 32
 CARACTERE_PRELAVA = 205
 NUMERO_LAVAS = 6
+PADDING = 4
 
 LavaP STRUCT
-	posX	byte	?
-	posY	byte	?
+	posX	byte	5
+	posY	byte	5
 	Stage	byte	0 ;0 = Seta novas posições ; 1 = Fica Vermelho; 2 = Expande; 3 = Seta o Growing pra 0;
 	Time	DWORD	0 ;0 = Começo; 50 = Estado 1; 100 = Estado 2; 200 = Estado 3; (valores sujeitos à alteração)
 	Growing	byte	1 ;Se growing = 0; então decrementa o estado
 LavaP ENDS
 
+
 .data
 	;mapa byte 119 DUP(32), 0 ; string de caracteres do chão
 
 	;Player
-	PosicaoX byte 1,1
+	PosicaoX byte 20,1
 	PosicaoY byte 14,1
 
 	;Tela
-	MaxX byte 129
+	MaxX byte 60
 	MaxY byte 28
 
 	;Lava
@@ -42,8 +55,8 @@ LavaP ENDS
 
 	; Relacionado à tela -----------------------------------
 	outHandle dword ?
-	titleStr byte "The Floor is Lava", 0
-	_small_rect SMALL_RECT <0, 0, 120, 29>
+	;titleStr byte "The Floor is Lava", 0
+	;_small_rect SMALL_RECT <0, 0, 84, 42>
 	;-------------------------------------------------------
 
 .code
@@ -67,11 +80,11 @@ Inicio PROC USES EAX EDX
 	mov    cursorInfo.bVisible,0
 	INVOKE SetConsoleCursorInfo, outHandle, ADDR cursorInfo
 ;---------------------------------------------------------------
- 	invoke getStdHandle, STD_OUTPUT_HANDLE
-	mov outHandle, eax
-	invoke setConsoleWindowInfo, outHandle, 1, addr _small_rect
-	invoke setConsoleTitle, addr titleStr	
-	call clrscr
+ 	;invoke getStdHandle, STD_OUTPUT_HANDLE
+	;mov outHandle, eax
+	;invoke setConsoleWindowInfo, outHandle, 1, addr _small_rect
+	;invoke setConsoleTitle, addr titleStr	
+	;call clrscr
 	
 ;---------------------------------------------------------------
 	; Salvando o X maximo 
@@ -146,23 +159,25 @@ Mover PROC
 	
 	ret
 	MoverCima:
-		cmp PosicaoY, 1
+		cmp PosicaoY, 2
 		je FimMove
 		dec PosicaoY
 		ret
 	MoverBaixo:
 		mov dh, MaxY
+		dec dh
 		cmp dh, PosicaoY
 		je FimMove
 		inc PosicaoY
 		ret
 	MoverEsq:
-		cmp PosicaoX, 1
+		cmp PosicaoX, 2
 		je FimMove
 		dec PosicaoX
 		ret
 	MexerDireita:
 		mov dl, MaxX
+		dec dl
 		cmp dl, PosicaoX
 		je FimMove
 		inc PosicaoX
@@ -180,24 +195,30 @@ ImprimirPersonagem PROC USES EDX EAX
 	jne ImprimirEfetivamente ; Jump para imprimir caso tenha mudado mesmo a posição
 	ret
 	ImprimirEfetivamente:
+
 	 	; Apagando a posicão antiga
-		ImprimirEm PosicaoX[1], PosicaoY[1], CARACTERE_ESPACO
-		; Escrever na posição nova
-		ImprimirEm PosicaoX, PosicaoY, CARACTERE_PERSONAGEM
-		; Salvar a posicao antiga
-		mov PosicaoX[1], dl
-		mov PosicaoY[1], dh
-		ret
+		mImprimirEm PosicaoX[1], PosicaoY[1], CARACTERE_ESPACO
+ 		; Escrever na posição nova
+		mImprimirEm PosicaoX, PosicaoY, CARACTERE_PERSONAGEM
+ 		; Salvar a posicao antiga
+ 		mov PosicaoX[1], dl
+ 		mov PosicaoY[1], dh
+		 ret
 ImprimirPersonagem endp
 
 LoopLavas PROC uses ecx
-	mov ecx, NUMERO_LAVAS
+	mov ecx, NUMERO_LAVAS-1
 	CadaLava:
-		test Lavas[ecx].Stage
-		jnz Estagio1
-		
-		Estagio1:
 		inc Lavas[ecx].Time;
+		cmp Lavas[ecx].Stage,0
+		jnz Estagio1
+		Estagio0:			
+			mGerarAleatorio8 MaxY, Lavas[ecx].posY
+			mGerarAleatorio8 MaxX, Lavas[ecx].posX
+			mImprimirEm Lavas[ecx].posX, Lavas[ecx].posY, CARACTERE_PRELAVA
+			mov Lavas[ecx].Stage, 1
+			ret
+		Estagio1:
 	loop CadaLava
 	ret
 LoopLavas endp
