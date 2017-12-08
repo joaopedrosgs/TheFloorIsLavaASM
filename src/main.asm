a7b11b5be971790548ee5a203ccc9782acd27da7
João Pedro <jsangregorio@hotmail.com>
2017-12-08 15:24:58 -0200

    preparativos para inserir as lavas

--- src/main.asm
+++ src/main.asm
@@ -1,21 +1,49 @@
 Include ..\Irvine32.inc
 		  
+ImprimirEm MACRO X, Y, CARACTERE
+	mov dl, X
+	mov dh, Y
+	call Gotoxy
+	mov al, CARACTERE
+	call WriteChar
+ENDM   
+
 CARACTERE_PERSONAGEM = 254
 CARACTERE_ESPACO = 32
 CARACTERE_PRELAVA = 205
+NUMERO_LAVAS = 6
+
+LavaP STRUCT
+	posX	byte	?
+	posY	byte	?
+	Stage	byte	0 ;0 = Seta novas posições ; 1 = Fica Vermelho; 2 = Expande; 3 = Seta o Growing pra 0;
+	Time	DWORD	0 ;0 = Começo; 50 = Estado 1; 100 = Estado 2; 200 = Estado 3; (valores sujeitos à alteração)
+	Growing	byte	1 ;Se growing = 0; então decrementa o estado
+LavaP ENDS
 
 .data
 	;mapa byte 119 DUP(32), 0 ; string de caracteres do chão
+
+	;Player
 	PosicaoX byte 1,1
 	PosicaoY byte 14,1
-	MaxX byte 1
+
+	;Tela
+	MaxX byte 129
 	MaxY byte 28
-	TempoLava dword 0
-	PosLava word 0
+
+	;Lava
 	LavaOn byte 0
+	Lavas LavaP NUMERO_LAVAS DUP(<>)
+
 	; Relacionado ao cursor --------------------------------
 	cursorInfo CONSOLE_CURSOR_INFO <>
-	outHandle  DWORD ?	
+	;-------------------------------------------------------
+
+	; Relacionado à tela -----------------------------------
+	outHandle dword ?
+	titleStr byte "The Floor is Lava", 0
+	_small_rect SMALL_RECT <0, 0, 120, 29>
 	;-------------------------------------------------------
 
 .code
@@ -26,19 +54,25 @@ LOOP_PRINCIPAL:
 	call Delay           
 	call Mover
 	call ImprimirPersonagem
-	call PontoDeLava
-	call AvancaLava
+	call LoopLavas
 	jmp LOOP_PRINCIPAL
 	exit
 	main endp
 
 Inicio PROC USES EAX EDX
 ;----- esconder o cursor ---------------------------------------
-   INVOKE GetStdHandle, STD_OUTPUT_HANDLE
-   mov    outHandle,eax
-   INVOKE GetConsoleCursorInfo, outHandle, ADDR cursorInfo
-   mov    cursorInfo.bVisible,0
-   INVOKE SetConsoleCursorInfo, outHandle, ADDR cursorInfo
+	INVOKE GetStdHandle, STD_OUTPUT_HANDLE
+	mov    outHandle,eax
+	INVOKE GetConsoleCursorInfo, outHandle, ADDR cursorInfo
+	mov    cursorInfo.bVisible,0
+	INVOKE SetConsoleCursorInfo, outHandle, ADDR cursorInfo
+;---------------------------------------------------------------
+ 	invoke getStdHandle, STD_OUTPUT_HANDLE
+	mov outHandle, eax
+	invoke setConsoleWindowInfo, outHandle, 1, addr _small_rect
+	invoke setConsoleTitle, addr titleStr	
+	call clrscr
+	
 ;---------------------------------------------------------------
 	; Salvando o X maximo 
 	call GetMaxXY
@@ -139,78 +173,33 @@ Mover endp
 ImprimirPersonagem PROC USES EDX EAX
 	;Pegando os dados pra checar se mudou a posição e pra apagar a posição antiga
 	mov dl, PosicaoX[1]
-	mov dh, PosicaoY[1]
 	cmp dl, PosicaoX
 	jne ImprimirEfetivamente ; Jump para imprimir caso tenha mudado mesmo a posição
+	mov dh, PosicaoY[1]
 	cmp dh, PosicaoY
 	jne ImprimirEfetivamente ; Jump para imprimir caso tenha mudado mesmo a posição
 	ret
 	ImprimirEfetivamente:
-		call Gotoxy
-		mov al, CARACTERE_ESPACO
-		call WriteChar ; Apagando a posicão antiga
+	 	; Apagando a posicão antiga
+		ImprimirEm PosicaoX[1], PosicaoY[1], CARACTERE_ESPACO
 		; Escrever na posição nova
-		mov dl, PosicaoX
-		mov dh, PosicaoY
-		call Gotoxy
-		mov al, CARACTERE_PERSONAGEM
-		call WriteChar
+		ImprimirEm PosicaoX, PosicaoY, CARACTERE_PERSONAGEM
 		; Salvar a posicao antiga
 		mov PosicaoX[1], dl
 		mov PosicaoY[1], dh
 		ret
 ImprimirPersonagem endp
-PontoDeLava PROC
-	cmp LavaOn, 0
-	ja fimp
-	mov dx, PosLava
-	call Gotoxy
-	mov al, CARACTERE_ESPACO
-	call WriteChar
-	call Randomize
-	mov eax, 30
-	call RandomRange
-	mov dh, al
-	add dh, 4
-	mov eax, 80
-	call RandomRange
-	add dl, 4
-	mov dl, al
-	mov PosLava, dx
-	call Gotoxy
-	mov eax, yellow
-	call SetTextColor
-	mov al, CARACTERE_PRELAVA
-	call WriteChar
-	mov eax, white
-	call SetTextColor
-	call GetMseconds
-	mov TempoLava, eax
-	mov LavaOn, 1
-	fimp:
-		ret
-PontoDeLava endp
-AvancaLava PROC
-	call GetMseconds
-	sub eax, TempoLava
-	cmp eax, 10000
-	ja some
-	cmp eax, 5000
-	ja mudaCor
+
+LoopLavas PROC uses ecx
+	mov ecx, NUMERO_LAVAS
+	CadaLava:
+		test Lavas[ecx].Stage
+		jnz Estagio1
+		
+		Estagio1:
+		inc Lavas[ecx].Time;
+	loop CadaLava
 	ret
-	mudaCor:
-		mov dx, PosLava
-		call Gotoxy
-		mov eax, red
-		call SetTextColor
-		mov al, CARACTERE_PRELAVA
-		call WriteChar
-		mov eax, white
-		call SetTextColor
-		mov LavaOn, 2
-		ret
-	some:
-		mov LavaOn, 0
-	ret
-AvancaLava endp
+LoopLavas endp
+
 end main
